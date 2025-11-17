d141c905667b6ec7d747012247a77be0dd842d88
5mokshith <mokshithrao1481@gmail.com>
2025-11-17 13:58:34 +0530

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,7 @@
+import***REMOVED***type***REMOVED***{***REMOVED***NextConfig***REMOVED***}***REMOVED***from***REMOVED***"next";
+
+const***REMOVED***nextConfig:***REMOVED***NextConfig***REMOVED***=***REMOVED***{
+***REMOVED******REMOVED***/****REMOVED***config***REMOVED***options***REMOVED***here***REMOVED****/
+};
+
+export***REMOVED***default***REMOVED***nextConfig;
---
+++ postcss.config.mjs
@@ -0,0 +1,7 @@
+const***REMOVED***config***REMOVED***=***REMOVED***{
+***REMOVED******REMOVED***plugins:***REMOVED***{
+***REMOVED******REMOVED******REMOVED******REMOVED***"@tailwindcss/postcss":***REMOVED***{},
+***REMOVED******REMOVED***},
+};
+
+export***REMOVED***default***REMOVED***config;
